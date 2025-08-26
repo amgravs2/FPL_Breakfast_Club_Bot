b26b96e64b35abcdb3c3c7c6b20f71b4d938e48e
amgravs2 <aaron.m.graves@outlook.com>
2025-08-25 21:37:27 -0500

    Update bot.py

--- bot.py
+++ bot.py
@@ -2,6 +2,8 @@ import discord
 from discord.ext import commands
 import os
 
+DISCORD_BOT_TOKEN = os.getenv("DISCORD_BOT_TOKEN")
+
 intents = discord.Intents.default()
 intents.message_content = True  # Required for reading messages
 
